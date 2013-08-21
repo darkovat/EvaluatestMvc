38a29472cbdb6719a0ed586ebabedb8db1643691
Ramiro Leonides Pacheco León <ramiro.pacheco@manoderecha.mx>
2013-08-21 01:06:17 -0500

    Sección ADN

--- application/views/va_conmigo/adn.php
+++ application/views/va_conmigo/adn.php
@@ -3,16 +3,56 @@
 		<div id='contenido2' class='sixteen columns'>
 			<div class='ten columns omega'>
 				<div id='seccion'>
-					<h2 class='titleB color_orange'>Evaluatest Va Conmigo</h2>
+					<h2 class='titleB color_orange'>ADN Brivé&reg;</h2>
 					<p>
-						Evaluatest® Suite es una poderosa herramienta de apoyo al toma de decisiones con relación al Capital Humano en lo Individual, Grupal, Organizacional que le permite lograr un Alto Desempeño de sus colaboradores en el negocio.
+						<strong>El talento es la diferencia.</strong>
 					</p>
 					<p>
-						Es una herramienta estratégica para la Dirección, Recursos Humanos y Gerencias ya que con la información y conocimiento que le provee pueden apoyar sus decisiones y coordinar mejor sus esfuerzos de Capacitación, Desarrollo Organizacional, Reclutamiento y Selección a fin de mejorar la productividad de la Compañía.
+						Nuestro equipo lo integran Ejecutivos y Consultores especialistas en Psicología, Estadística, Desarrollo Humano, Visión de Negocio y Tecnología.					
 					</p>
 					<p>
-						Es la solución líder que le proporciona conocimiento objetivo para lograr un Alto Desempeño del Capital Humano en su negocio.
+						Nuestra operación y servicios se rigen por:
+						<ul class='list'>
+							<li>Orientación en sus Resultados.</li>
+							<li>Metodologías y procesos.</li>
+							<li>Disposición y actitud de servicio.</li>
+							<li>Mejor relación costo beneficio.</li>
+							<li>Pasión por el Cliente.</li>
+							<li>Experiencia en mejores prácticas.</li>
+							<li>Soluciones apoyadas en la mejor tecnología.</li>
+							<li>Clientes satisfechos.</li>
+							<li>Certidumbre para dar resultados.</li>
+							<li>Objetividad y Confiabilidad.</li>
+						</ul>
 					</p>
+					<p>
+						Nuestros Consultores forman parte de la SHRM Society for Human Resource Management.
+					</p>
+					<br />
+					<p>
+						<strong>Metodologías y Mejores Prácticas</strong>
+						<ol class='list-ol'>
+							<li>
+								Nuestros servicios y soluciones se apegan a metodologías y mejores prácticas en la industria a nivel mundial.
+							</li>
+							<li>
+								Participamos en Foros y Conferencias a nivel Internacional que nos mantienen a la vanguardia para traer a ustedes las mejores prácticas de referencia a favor de su negocio.
+							</li>
+							<li>
+								Nuestros servicios son operados por Ejecutivos certificados internamente para aplicar la metodología y ejecutar un proceso de trabajo.
+							</li>
+							<li>
+								Nuestros Consultores son afiliados activos de la SHRM Society for Human Resources Management donde participan otros especialistas a nivel mundial en foros de opinión.
+							</li>
+							<li>
+								Contamos con un Consejo de Expertos integrado por Directores y Especialistas de RH y Negocios quienes contribuyen con su experiencia en los procesos de mejora dentro de nuestra Compañía.
+							</li>
+						</ol>
+					</p>
+					<p>
+						Nuestra Compañía valora las relaciones de sinergia para dar mayor valor a nuestros Clientes comunes.
+					</p>
+					<br /><br />	
 				</div>
 			</div>
 			<div class='six columns alpha'>
@@ -21,24 +61,6 @@
 				</div>
 				<p class='color_blue_dark pie'>Conoce Evaluatest, Vivelo y se parte del cambio</p>
 				<br />
-				<ul id='menu-sidebar'>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Funcionamiento de Diseño y Desarrollo</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Casos de éxito</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Servicios adicionales</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Dirigido para:</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Ficha técnica del producto</a>
-					</li>
-				</ul>
-				
 				<div class='info'>
 					<a class='btn_info bg_orange' href="">
 						Chat en vivo
