81d4ebc260180a4da80f78138e586c474d50d7aa
Ramiro Leonides Pacheco León <ramiro.pacheco@manoderecha.mx>
2013-08-21 01:32:03 -0500

    Sección Socio_Negocios

--- application/views/va_conmigo/socio_de_negocios.php
+++ application/views/va_conmigo/socio_de_negocios.php
@@ -3,16 +3,56 @@
 		<div id='contenido2' class='sixteen columns'>
 			<div class='ten columns omega'>
 				<div id='seccion'>
-					<h2 class='titleB color_orange'>Evaluatest Va Conmigo</h2>
+					<h2 class='titleB color_orange'>Convertirse en Socio de Negocios</h2>
 					<p>
-						Evaluatest® Suite es una poderosa herramienta de apoyo al toma de decisiones con relación al Capital Humano en lo Individual, Grupal, Organizacional que le permite lograr un Alto Desempeño de sus colaboradores en el negocio.
+						Expanda sus posibilidades de Negocio, nuestra Compañía cuenta con “Evaluatest® Partner Program” para México y Latinoamérica que le ofrece las siguientes modalidades:
 					</p>
 					<p>
-						Es una herramienta estratégica para la Dirección, Recursos Humanos y Gerencias ya que con la información y conocimiento que le provee pueden apoyar sus decisiones y coordinar mejor sus esfuerzos de Capacitación, Desarrollo Organizacional, Reclutamiento y Selección a fin de mejorar la productividad de la Compañía.
+						<ol class='list-ol'>
+							<li>Afiliado de Negocio.</li>
+							<li>Representante Comercial Certificado.</li>
+							<li>Concesionario Certificado.</li>
+						</ol>
 					</p>
 					<p>
-						Es la solución líder que le proporciona conocimiento objetivo para lograr un Alto Desempeño del Capital Humano en su negocio.
+						Para la aplicar a cualquier modalidad le solicitamos nos escriba un correo a sociosdenegocio@evaluatest.com que contenga la siguiente información:
+						<ol class='list-ol'>
+							<li>Datos del contacto.<br /><br />
+								<ul class='list'>
+									<li>Nombre completo</li>
+									<li>Empresa, Negocio o Despacho</li>
+									<li>Puesto</li>
+									<li>Dirección</li>
+									<li>Teléfono</li>
+									<li>Cuenta de correo</li>
+									<li>Sitio web</li>
+								</ul>
+							</li>
+							<li>Indique la oportunidad de negocio que identifica<br /><br />
+								<ul class='list'>
+									<li>Cuál es su interés en la relación con Brivé</li>
+									<li>Qué requiere de apoyo de Brivé</li>
+									<li>Qué ofrece usted en la relación con Brivé</li>
+									<li>Anexe su CV y/o el de su Empresa.</li>
+									<li>Teléfono</li>
+									<li>Cuenta de correo</li>
+									<li>Sitio web</li>
+								</ul>
+							</li>		
+						</ol>
 					</p>
+					<p>
+						Un Ejecutivo le contactará para ofrecerle información sobre la mejor opción de Negocio y sinergia entre nosotros:
+						<ul class='list'>
+							<li>Explicación de cada modalidad del Programa</li>
+							<li>Beneficios</li>
+							<li>Costo de Afiliación al Programa</li>
+							<li>Certificación para operar</li>
+							<li>Siguientes pasos</li>
+						</ul>
+					</p>
+					<p>Su correo será respondido en las siguientes 72 horas hábiles y un Ejecutivo le contactará personalmente.</p>
+					<br /><br />
 				</div>
 			</div>
 			<div class='six columns alpha'>
@@ -21,23 +61,6 @@
 				</div>
 				<p class='color_blue_dark pie'>Conoce Evaluatest, Vivelo y se parte del cambio</p>
 				<br />
-				<ul id='menu-sidebar'>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Funcionamiento de Diseño y Desarrollo</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Casos de éxito</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Servicios adicionales</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Dirigido para:</a>
-					</li>
-					<li class='bg_style'><div class='more'></div>
-						<a class='color_blue' href="">Ficha técnica del producto</a>
-					</li>
-				</ul>
 				
 				<div class='info'>
 					<a class='btn_info bg_orange' href="">
